675cd91dba5fc82026c12608f46a5deb5042a96c
flaxmans <sam.flaxman@gmail.com>
2019-01-15 21:58:34 -0700

    added first names to roster parsing script

--- ExampleScripts/MakeStudentFolders.sh
+++ ExampleScripts/MakeStudentFolders.sh
@@ -14,18 +14,19 @@ cd ~/Downloads
 cat EBIO-4420-010.csv > CompBioRoster.csv # undergrad roster in new file
 tail -n +2 EBIO-5420-010.csv >> CompBioRoster.csv # grad students appended minus header
 
-# get list of last names with punctuation and header trimmed off
+# get list of names with punctuation and header trimmed off
 # and store the list in a variable for use in the shell
-list=`tail -n +2 CompBioRoster.csv | cut -f 1 -d, | cut -f 2 -d'"'`
+list=`tail -n +2 CompBioRoster.csv | cut -f 1-2 -d, | cut -f2 -d'"' | sed 's/, /_/'`
+# this makes a list with each student represented as Lastname_Firstname
 
-# make directories for student work using last names
+# make directories for student work using names
 # do so in the directory I use for tracking student work
 cd ~/Documents/Teaching/ComputationalBiology/StudentWork2018/
 for i in $list
 do
-	mkdir $i # make directory for each student using their last name
+	mkdir $i # make directory for each student using their name
 done
 
-# save list of last names in a file for future use:
-echo $list > LastNames.txt
+# save list of names in a file for future use:
+echo $list > StudentNames.txt
 
