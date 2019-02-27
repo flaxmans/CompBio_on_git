a269c93d60c1fb095be5d6dff964abb620c00100
flaxmans <sam.flaxman@gmail.com>
2019-02-27 11:29:27 -0700

    modified example script for Lab07 to more closely match lecture content

--- Labs/Lab07/ExampleScriptSetup.R
+++ Labs/Lab07/ExampleScriptSetup.R
@@ -14,16 +14,24 @@
 # Hypothetical Problem (NOT a real problem in the actual lab)
 # Write a function that calculates the hypotenuse of a right triangle
 # when given the other two sides:
-pythagTheorem <- function( side1 = NA, side2 = NA ) {
-  return( sqrt( (side1 ^ 2) + (side2 ^ 2) ) )
+myHypFunc <- function(a, b) {
+  # two sides, a and b, must be given
+  # square one side (a^2), square the second side (b^2), and
+  # then sum those two squares
+  mysum <- a^2 + b^2
+  # take the square root of that sum
+  myHypotenuse <- sqrt( mysum )
+  return( myHypotenuse )
 }
 
 # THEN, give test cases of the function's usage:
 
 # Demonstrate that it works:
 # Suppose a triangle with sides 3 and 4; we expect the hypotenuse to be 5
-pythagTheorem( 3, 4 )
-pythagTheorem( 30, 40 )
+myHypFunc(3, 4)
+myHypFunc(30, 40)
+myHypFunc(10, 10)
+sqrt(2) == myHypFunc(1,1)
 
 # Then, move on to the next problem
 
@@ -42,3 +50,4 @@ pythagTheorem( 30, 40 )
 
 # Problem #2.  Our own absolute value function
 # and so on ....
+
