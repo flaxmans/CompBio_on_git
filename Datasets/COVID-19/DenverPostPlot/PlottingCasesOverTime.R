dc0013ba514784ad6d26670ea9b19b1be7b19608
flaxmans <sam.flaxman@gmail.com>
2020-04-12 23:36:39 -0600

    updated data and scripts for Denver Post plots

--- Datasets/COVID-19/DenverPostPlot/PlottingCasesOverTime.R
+++ Datasets/COVID-19/DenverPostPlot/PlottingCasesOverTime.R
@@ -7,10 +7,9 @@ rm(list = ls())
 dpp <- read.csv( file = "~/compbio/CompBio_on_git/Datasets/COVID-19/DenverPostPlot/RevisedData.csv", stringsAsFactors = F)
 # convert date from char to date:
 dpp$Date <- as.Date( dpp$Date, format = "%m/%d/%y" )
-# take a look:
-head( dpp )
 # I'm going to change the names slightly to make it easier to parse later ...
-names(dpp)[2:3] <- c("New_Cases", "New_Deaths")
+names(dpp)[2:3] <- c("Daily_Cases", "Daily_Deaths")
+str(dpp)
 head(dpp)
 
 
@@ -25,57 +24,41 @@ head(dpp)
 #############################################
 
 # need some new variables created for the cumulative counts.  
-Total_Cases <- dpp$New_Cases      # preallocate with correct number in position 1
-Total_Deaths <- dpp$New_Deaths    # preallocate with correct number in position 1
-for ( day in 2:nrow(dpp) ) {
-  # loop to calculate cumulative totals
-  Total_Cases[day] <- sum(dpp$New_Cases[1:day])
-  Total_Deaths[day] <- sum(dpp$New_Deaths[1:day])
-}
+
 
 ###############################################################
 ## 2. Building data frames that work nicely for ggplot
 ###############################################################
 
 ## Method 1 for building one data frame: "spread out" or "wide form"
-dppSpread <- cbind(dpp, Total_Cases, Total_Deaths)
-head(dppSpread)
+
 # note that every method of counting is in its own column/variable
 
 ## Method 2: stacked or "long form" using tidyr's "pivot_longer()"; 
 # pivot_longer() is a sort of updated version of gather().  See the
 # vignette at https://tidyr.tidyverse.org/articles/pivot.html 
-names(dppSpread) # note the pattern: two variables stored in four names, 
-# with variable values separated by underscore
+ 
+# note the pattern: two variables stored in four names, 
 
-require(tidyr)
 
-dppStacked <- dppSpread %>%
-  pivot_longer( cols = 2:5,
-                names_to = c("CountType", "Outcome"),
-                names_sep = "_",
-                values_to = "Count")
-
-head(dppStacked)
 
 ##################################################################
 ## 3. Actually Plotting
 ##################################################################
+
+require(ggplot2)
+
 # Sometimes, the spread form is nice, like when you only want 
 # to plot a subset of the data.  Note that data differentiation
 # occurs through calls to multiple geom's:
-require(ggplot2)
-ggplot( dppSpread ) +
-  geom_line( aes(x = Date, y = Total_Cases), color = "blue") + 
-  geom_bar( aes(x = Date, y = New_Cases ), stat = "identity", fill = "blue")
+
+
 
 # The "stacked" form is nice when you want to let ggplot do the work of 
 # choosing colors, linetypes, etc., and you want all data to be displayed
 # with the SAME GEOM:
-ggplot( dppStacked ) + 
-  geom_line( aes(x = Date, y = Count, linetype = CountType, color = Outcome)) +
-  facet_wrap( ~Outcome, nrow = 2)  + 
-  scale_y_log10()
+
+
 
 # Which would be better/easier for reconstructing the Denver Post's plots?
 # And, how would we do it?
