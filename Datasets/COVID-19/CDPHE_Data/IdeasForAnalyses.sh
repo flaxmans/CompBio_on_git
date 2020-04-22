01ffa8eddba44a97a929a49a20a014af0113de7d
flaxmans <sam.flaxman@gmail.com>
2020-04-21 22:36:49 -0600

    data and script updates for April 21

---
+++ Datasets/COVID-19/CDPHE_Data/IdeasForAnalyses.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+##############################################
+# how about some aggregated data on Boulder?
+##############################################
+
+# Method 1: Wildcard grep
+grep "Boulder" RawData_csv_files/*.csv > Boulder_WildCardGrep.csv
+
+# That works, but makes a lot of stuff with punctuation that could be hard to parse.
+# We can use some tools to get rid of some unneeded bits:
+
+grep "Boulder" RawData_csv_files/*.csv |        # get all the data
+    sed 's/_/,/g' |                             # replace underscores with commas
+    sed 's/:/,/g' |                             # replace colons with commas
+    sed 's/.csv//g' > Boulder_WildCardGrep.csv  # remove instances of ".csv"
+
+# now we have a lot of punctuation removed, and the first 4 "columns"
+# are really unneed.  Useful info starts with the 5th "column"
+
+# same pipeline as last one but with an addition:
+grep "Boulder" RawData_csv_files/*.csv |        # get all the data
+    sed 's/_/,/g' |                             # replace underscores with commas
+    sed 's/:/,/g' |                             # replace colons with commas
+    sed 's/.csv//g' |                           # remove instances of ".csv"
+    cut -d, -f 5- > Boulder_WildCardGrep.csv
+    
+# Notice how the filenames from the original files are incorporated now as a useful
+# column that can easily be converted to a date!
+
+# Method 2: Loop over individual files and re-craft lines as desired
+# it's a possibility, but we won't do it here (at least, not yet)
+
+################################################
+# How about aggregated on multiple counties?
+################################################
+
+# We could try the same thing?
+grep "Case Counts by County" RawData_csv_files/*.csv |        # get all the data
+    sed 's/_/,/g' |                             # replace underscores with commas
+    sed 's/:/,/g' |                             # replace colons with commas
+    sed 's/.csv//g' |                           # remove instances of ".csv"
+    cut -d, -f 5- > AllCounties_WildCardGrep.csv
+
+
+
+##############################################
+# How about statewide data?
+##############################################
+grep "Statewide" RawData_csv_files/*.csv |        # get all the data
+    sed 's/_/,/g' |                             # replace underscores with commas
+    sed 's/:/,/g' |                             # replace colons with commas
+    sed 's/.csv//g' |                           # remove instances of ".csv"
+    cut -d, -f 5- > Statewide_WildCardGrep.csv
---
+++ Datasets/COVID-19/CDPHE_Data/PlotBoulderCountyData.R
@@ -0,0 +1,42 @@
+# Script for looking at COVID-19 data as provided in multiple files by CDPHE
+# See Shell script for aggregation of data from separate files into a single file
+
+# Boulder County Data
+Boulder_WildCard_data <- read.csv("~/compbio/CompBio_on_git/Datasets/COVID-19/CDPHE_Data/Boulder_WildCardGrep.csv", stringsAsFactors = F, header = F)
+str(Boulder_WildCard_data)
+names(Boulder_WildCard_data) <- c("Date", "Description", "Attribute", "Metric", "Value")
+Boulder_WildCard_data$Date <- as.Date(Boulder_WildCard_data$Date)
+
+# see what we have:
+# Options include unique(), table(), summarize()
+require(dplyr)
+Boulder_WildCard_data %>%
+  group_by( Attribute, Description, Metric ) %>%
+  summarize( NumberOfRows = n() )
+
+# remove rows with missing descriptors:
+filteredData <- Boulder_WildCard_data %>%
+  filter( Attribute != "" & Description != "" & Metric != "" )
+
+# change names to reflect our data on only Boulder County:
+changeCase <- filteredData$Description == "Case Counts by County"
+filteredData$Description[changeCase] <- "Cases in Boulder County"
+changeCase <- filteredData$Description == "Deaths"
+filteredData$Description[changeCase] <- "Deaths in Boulder County"
+
+require(ggplot2)
+# Plot only cases over time:
+ggplot( data = subset(filteredData, Metric == "Cases" & Description == "Cases in Boulder County") ) +
+  geom_line( mapping = aes(x = Date, y = Value))
+
+# plot cases and deaths but not rates per 100000
+ggplot( data = subset(filteredData, Metric == "Cases"), 
+        mapping = aes(x = Date, y = Value)) +
+  geom_smooth() +
+  geom_point() + 
+  facet_wrap( ~Description, scales = "free_y", nrow = 2 ) + 
+  labs( y = "Number" ) + 
+  theme_bw()
+  
+
+
