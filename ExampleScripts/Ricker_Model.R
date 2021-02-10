0f4d660728c240b84f109bc9fe3b31b88b5bfb87
flaxmans <sam.flaxman@gmail.com>
2021-02-10 09:12:20 -0700

    better comments in Ricker Model script

--- ExampleScripts/Ricker_Model.R
+++ ExampleScripts/Ricker_Model.R
@@ -1,4 +1,10 @@
-# Ricker Model:
+# Ricker Model: A discrete-time density dependent model of population growth
+
+# Ricker model equation as explained at:
+# https://en.wikipedia.org/wiki/Ricker_model
+
+# See also:
+# Ricker, W. E. (1954) Stock and Recruitment Journal of the Fisheries Research Board of Canada, 11(5): 559-623. doi:10.1139/f54-039
 
 # parameters:
 startTime <- 0
@@ -9,23 +15,23 @@ growthRate <- 0.05
 
 # make data objects:
 time <- startTime:endTime
-abundance <- rep(initialAbundance, times = length(time))
+abundance <- rep(initialAbundance, times = length(time)) # pre-allocation
 
 # loop to calculate:
-for ( i in 2:length(time) ) {
-  # Ricker model equation as explained at:
-  # https://en.wikipedia.org/wiki/Ricker_model
-  abundance[i] <- abundance[i-1] * exp( growthRate * (1 - (abundance[i-1]/carryingCap)) )
+for ( i in 2:length(time) ) {   
+  # note that we start at index 2, because index 1 is the initial
+  # abundance at time zero; put another way, note that 
+  # length(time) is endTime + 1 because startTime is zero
   
-  # See also:
-  # Ricker, W. E. (1954) Stock and Recruitment Journal of the Fisheries Research Board of Canada, 11(5): 559???623. doi:10.1139/f54-039Ricker, W. E. (1954) Stock and Recruitment Journal of the Fisheries Research Board of Canada, 11(5): 559???623. doi:10.1139/f54-039
+  # this is the Ricker equation from the link above:
+  abundance[i] <- abundance[i-1] * exp( growthRate * (1 - (abundance[i-1]/carryingCap)) )
+
 }
 
-# visualize results:
+# visualize results with base R:
 plot( time, abundance, type = "b")
 
-
-# With ggplot:
+# visualize with ggplot:
 library(ggplot2)
 ggplot( data = data.frame(time,abundance) ) +
   geom_line( mapping = aes( x = time, y = abundance) )
