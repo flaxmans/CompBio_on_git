b69c818156338b287d0ae09b78c47e8fef440c1f
flaxmans <sam.flaxman@gmail.com>
2021-02-15 12:11:44 -0700

    added average rainfall by state data and small script

---
+++ Datasets/AverageRainfallByState/README.md
@@ -0,0 +1,7 @@
+# Average Rainfall By State
+
+Data copied and pasted from 
+[https://www.currentresults.com/Weather/US/average-annual-state-precipitation.php](https://www.currentresults.com/Weather/US/average-annual-state-precipitation.php), 
+accessed 2/14/21
+
+
---
+++ Datasets/AverageRainfallByState/SubsettingWithConditionals.R
@@ -0,0 +1,37 @@
+# Some data on rainfall averages by state across the USA:
+rainfall <- read.csv( file = "~/compbio/CompBio_on_git/Datasets/AverageRainfallByState/AverageRainfallByState.csv", 
+                      stringsAsFactors = F)
+
+# General kind of problem:  Subset the data based upon the numbers
+
+# Here are some questions to answer using conditionals:
+# How many states receive an average annual rainfall of more than 1 meter, and which states are they?
+# Which states are closest to the median rainfall among all states?
+# What are the 10 states that get the least rainfall?
+
+# Is a sort necessary?
+# Millimeters to meters conversion
+# Find rows where millimeters > 1000
+rainThreshold <- 500 # 1 meter is 1000 millimeters
+# Look at third column (Millimeters) and Find rows where millimeters > 1000
+
+rainfall[,3] > rainThreshold
+rainfall$Millimeters > rainThreshold
+
+# Cross-reference rows from previous step to State names
+# get the indexes that satisfy previous line
+indexes <- which(rainfall$Millimeters > rainThreshold)
+
+logicalIndexes <- rainfall$Millimeters > rainThreshold
+
+# use indexes to get the states
+states1 <- rainfall[ indexes, 1 ]
+states2 <- rainfall$State[ indexes ]
+states3 <- rainfall$State[ logicalIndexes ]
+
+
+# Count number states satisfying our criterion
+numStates <- length( states1 )
+
+# Print out the names
+
