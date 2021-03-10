5957b1018e6e8451a5c72c4bbbee76afbe4235ef
flaxmans <sam.flaxman@gmail.com>
2021-03-10 09:49:35 -0700

    added a bit more to import script for broken files

--- Datasets/FilesThatNeedFixing/ImportsThatDoNotWork.R
+++ Datasets/FilesThatNeedFixing/ImportsThatDoNotWork.R
@@ -4,6 +4,9 @@ setwd("~/compbio/CompBio_on_git/Datasets/FilesThatNeedFixing/")
 # Try each and inspect the results
 # see the README at https://github.com/flaxmans/CompBio_on_git/tree/main/Datasets/FilesThatNeedFixing for more details
 
-edgeList <- read.table("EdgeList.txt", sep = "\t")
 
-mooresLawData <- read.csv("moore.csv")
+edgeList <- read.table("EdgeList.txt", sep = "\t") # should import with three columns
+
+mooresLawData <- read.csv("moore.csv") # should import with six columns
+
+chromosomeNumbers <- read.table("ChromosomeNumber.txt", sep = "\t") # should import with six columns
