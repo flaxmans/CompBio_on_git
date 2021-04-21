041aed051a9aae54392185f6a7cb1993f08d8df4
flaxmans <sam.flaxman@gmail.com>
2021-04-20 20:06:27 -0600

    some udpates to merge and join exercises

--- Datasets/JoiningMultipleData/MergeAndJoinPlayground.R
+++ Datasets/JoiningMultipleData/MergeAndJoinPlayground.R
@@ -40,7 +40,7 @@ require("dplyr")
 # taken one at a time, what do we learn from each?
 # note we are not even telling  dplyr's functions what to join "by"
 inner_join( x = Sites1, y = Sites3 ) # inner_join is also sometimes called "natural join"
-inner_join( Sites3, Sites1 ) # occassionally inner_join is called "join"
+inner_join( x = Sites3, y = Sites1 ) # occassionally inner_join is called "join"
 
 right_join( x = Sites1, y = Sites3 ) # also sometimes called "right outer join"
 right_join( x = Sites3, y = Sites1 )
@@ -50,6 +50,8 @@ left_join( x = Sites3, y = Sites1 )
 
 full_join( x = Sites1, y = Sites3 ) # full_join is also sometimes known as "outer join" or "full outer join"
 full_join( x = Sites3, y = Sites1 )
+
+
 
 ###############################################################################
 ## For this data set, what makes the most sense in terms of joining all three?
@@ -65,6 +67,36 @@ full_join( x = Sites3, y = Sites1 )
 
 
 
+##########################################################################
+## Changing the "by" argument.  Compare:
+
+full_join( x = Sites1, y = Sites3, by = "SiteID" )
+full_join( x = Sites1, y = Sites3, by = c("SiteID", "AvgTempC" ))
+full_join( x = Sites1, y = Sites3 )
+
+
+
+##################################################################################
+## What about some different columns
+
+ExperimentalData <- read.csv("ExperimentalData.csv")
+allSites
+ExperimentalData
+
+ij <- inner_join(allSites, ExperimentalData, by = "SiteID")
+rj <- right_join(allSites, ExperimentalData, by = "SiteID")
+lj <- left_join(allSites, ExperimentalData, by = "SiteID")
+fj <- full_join(allSites, ExperimentalData, by = "SiteID")
+
+ij 
+rj
+lj
+fj
+
+ij[ is.na(ij) ] <- 0
+rj[ is.na(rj) ] <- 0
+all(ij == rj)
+
 ################################################################################
 ##  Suppose we had some updates
 ## Would any of the following help?
@@ -72,6 +104,8 @@ full_join( x = Sites3, y = Sites1 )
 SitesUpdate <- read.csv("SitesUpdate.csv", stringsAsFactors = F)
 # note it has updates to some rows and some brand new sites
 
+# None of the basic joins give us exactly what we might want, though we could work with them.
+# Look at the results of each:
 inner_join( allSites, SitesUpdate )
 
 full_join( allSites, SitesUpdate )
@@ -89,8 +123,32 @@ right_join( allSites, SitesUpdate, by = "SiteID" )
 left_join( allSites, SitesUpdate, by = "SiteID" )
 
 
-anti_join( SitesUpdate, allSites, by = "SiteID" )
-anti_join( allSites, SitesUpdate, by = "SiteID" )
+# Here is another join offered by dplyr.  Could we use this for updating?
 
-semi_join( allSites, SitesUpdate, by = "SiteID" )
-semi_join( SitesUpdate, allSites, by = "SiteID" )
+anti_join( x = SitesUpdate, y = allSites )  # rows unique to x
+anti_join( x = SitesUpdate, y = allSites, by = "SiteID" )  # rows with SiteID unique to x
+anti_join( x = allSites, y = SitesUpdate )
+anti_join( x = allSites, y = SitesUpdate, by = "SiteID" )
+
+# Here's one idea
+
+NoChangeSites <- anti_join( allSites, SitesUpdate, by = "SiteID" )
+NoChangeSites
+UpdatedAllSites <- full_join( NoChangeSites, SitesUpdate ) %>% arrange( SiteID )
+UpdatedAllSites
+
+#################################################################
+## APPENDIX
+
+#############################################
+## some basic set operations
+
+x <- LETTERS[1:4]
+y <- LETTERS[3:7]
+
+x
+y
+intersect( x, y )
+setdiff( x, y )
+setdiff( y, x )
+union( x, y )
